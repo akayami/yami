27a8249b38041874c70f1e25c2ca4e765fbcc126
Tomasz Rakowski <akayami@gmail.com>
2012-04-24 16:23:34 -0400

    Added fall back route called auto to automatically route to controller/action specified in the URI.
    Need to check security on this - Not really recommended to use this. 
    Routes should be declared explicitly as much as possible.

---
+++ yami/lib/yami/Router/Route/Auto.php
@@ -0,0 +1,69 @@
+<?php
+namespace yami\Router\Route;
+use yami\Http\Request;
+
+use yami\Router\Route;
+
+class Auto extends Regex {
+
+	protected $regex;
+	public $matches;
+	protected $namespace;
+	
+	/**
+	 * 
+	 */
+	public function __construct($defaultControllerNamespace = '') {
+		parent::__construct('#/(?<controller>\w+)/(?<action>\w+)(?<uriparams>.*)$#', '', '');
+		$this->namespace = $defaultControllerNamespace;
+	}	
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see yami\Router.Route::isValid()
+	 */
+	public function isValid($request) {
+		if($result = parent::isValid($request)) {
+			$this->controller = $this->matches['controller'];
+			$this->action = $this->matches['action'];
+			if(isset($this->matches['uriparams'])) {
+				$this->matches = array_merge($this->matches, $this->parseURI($this->matches['uriparams']));
+			}
+		}
+		return $result;		
+	}
+	
+	protected function parseURI($uri) {
+		$list = explode('/', trim($this->matches['uriparams'], '/'));
+		$i = 2;
+		$values = array();
+		foreach($list as $element) {
+			if($i % 2 == 0) {
+				$key = $element;
+			} else {
+				$values[$key] = $element;
+			}
+			$i++;
+		}
+		return $values;
+	}
+	
+	public function handle() {
+		$incPaths = explode(PATH_SEPARATOR, get_include_path());
+		foreach($incPaths as $path) {
+			$file = $path.DIRECTORY_SEPARATOR.str_replace('\\', DIRECTORY_SEPARATOR, $this->namespace).DIRECTORY_SEPARATOR.$this->controller.'.php';
+			if(($real = realpath($file)) !== false) {				
+				$object = $this->namespace.'\\'.$this->controller;
+				$a = new $object($this->action);
+				if(method_exists($a, $this->action)) {
+					$result = $a->{$this->action}();
+				} else {
+					throw new \yami\Router\Exception('Not Found', 404);
+				}
+				$a->render();
+				return $result;
+			}
+		}
+		throw new \yami\Router\Exception('Not Found', 404);
+	}
+}

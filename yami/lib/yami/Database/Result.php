fa08463b73b510ef84cb293722f045ddb7bceba9
Tomasz Rakowski <akayami@gmail.com>
2012-11-25 11:36:55 -0500

    Added fetchCol quick method to extract a single column as one dimentional array of data.

--- yami/lib/yami/Database/Result.php
+++ yami/lib/yami/Database/Result.php
@@ -2,50 +2,58 @@
 namespace yami\Database;
 
 interface Result {
-	
+
 	const FETCH_ASSOC = 1;
 	const FETCH_NUM = 2;
-	const FETCH_BOTH = 3;	
-	
+	const FETCH_BOTH = 3;
+
 	/**
-	 * 
+	 *
 	 * Enter description here ...
 	 * @return array
 	 */
 	public function fetchAll();
-	
+
 	/**
-	 *  
+	 *
 	 * @return array
 	 */
 	public function fetch();
 
 	/**
-	 * 
+	 *
 	 * @return int
 	 */
 	public function length();
-	
-	
+
+
 	/**
 	 * Returns the list of fields
-	 * 
+	 *
 	 * @return array
 	 */
 	public function fields();
-	
-	
+
+
 	/**
 	 * Number of affected rows
-	 * 
+	 *
 	 * @return int
 	 */
 	public function affectedRows();
-	
-	
+
+
 	/**
 	 * Sets the fetch mode
 	 */
 	public function fetchMode($arg);
-	
+
+
+	/**
+	 * Fetch a single column as a one dimentional array
+	 *
+	 * @param string $columnName
+	 */
+	public function fetchCol($columnName);
+
 }
--- yami/lib/yami/Database/Result/CommonResult.php
+++ yami/lib/yami/Database/Result/CommonResult.php
@@ -11,5 +11,20 @@ abstract class CommonResult implements Result {
 		$this->fetchMode = $arg;
 	}
 
+	/**
+	 * (non-PHPdoc)
+	 * @see \yami\Database\Result::fetchCol()
+	 */
+	public function fetchCol($columnName) {
+		$output = array();
+		while($row = $this->fetch()) {
+			if(isset($row[$columnName])) {
+				$output[] = $row[$columnName];
+			} else {
+				throw new \Exception('Specified column name not found:'.$columnName);
+			}
+		}
+		return $output;
+	}
 
 }

ba0ae5f02776fb9cbb0087c26c7c5f55540b4d91
Tomasz Rakowski <akayami@gmail.com>
2012-11-25 11:40:06 -0500

    Better handling of variable accessing in views.

--- yami/lib/yami/View.php
+++ yami/lib/yami/View.php
@@ -4,23 +4,23 @@ namespace yami;
 use yami\Misc\ArrayObject;
 
 class View extends ArrayObject {
-	
+
 	protected $action;
-	
+
  	public function __construct(array $data = array()) {
  		$this->__data = $data;
  	}
-	
+
  	public function get($key, $default = null) {
  		if(parent::offsetExists($key)) {
  			return parent::offsetGet($key);
  		} elseif(!is_null($default)) {
  			return $default;
  		} else {
- 			throw new \ErrorException('Undefined index: '.$key);
- 		} 	
+ 			throw new \Exception('Undefined index: '.$key);
+ 		}
  	}
-	
+
 	public function render() {
 		ob_start();
 		if(!include($this->action.'.phtml')) {
@@ -30,21 +30,21 @@ class View extends ArrayObject {
 		ob_end_clean();
 		echo $out;
 	}
-	
+
 	public function setActionName($string) {
 		$this->action = $string;
 	}
-	
+
 	/**
-	 * 
-	 * 
+	 *
+	 *
 	 * @param filepath $path  A filepaht within the include path available
 	 * @param array $data	  Provide dataset to use if different than within the main scope
 	 */
 	public function inject($path, array $data = null) {
 		if(is_null($data)) {
 			if(!include($path.'.phtml')) {
-				throw new \Exception('Failed to include: '.$path.'.phtml');	
+				throw new \Exception('Failed to include: '.$path.'.phtml');
 			}
 		} else {
 			$v = new Static($data);
